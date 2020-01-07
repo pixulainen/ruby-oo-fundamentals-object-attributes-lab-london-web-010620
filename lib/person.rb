21b377eae355e63a968cb61cd06b572a0fc0b544
George Apetrei <george.apetrei@gmail.com>
2020-01-07 14:37:03 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,27 @@
+class Person
+    
+        def job
+            @job = job
+        end
+        def name
+            @name = name
+        end
+        def job=(job)
+            @job = job
+        end
+        def name=(name)
+            @name = name
+        end
+    
+        def name
+        @name
+        end
+        def name 
+        @name
+        end
+        
+        def job
+        @job
+        end
+        
+  end
