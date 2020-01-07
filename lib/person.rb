7b412c4885d5f350ac401c43852591d40754f0a4
George Apetrei <george.apetrei@gmail.com>
2020-01-07 14:30:29 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,17 @@
 class Person
     
-    def job
-        @job = job
-    end
-    def name
-        @name = name
-    end
-    def job=(job)
-        @job = job
-    end
-    def name=(name)
-        @name = name
-    end
+      def job
+          @job = job
+      end
+      def name
+          @name = name
+      end
+      def job=(job)
+          @job = job
+      end
+      def name=(name)
+          @name = name
+      end
     
         def name
         @name
